f4e19746d0f48ad85e9799625a7e701e0cd799bd
Bikram-123 <bikramjarmal111.bj@gmail.com>
2020-12-09 10:59:49 +0530

    hello

---
+++ fontend/src/App.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import { BrowserRouter, Route, Link} from 'react-router-dom';
+import './App.css';
+import CartScreen from './screens/CartScreen';
+import SigninScreen from './screens/SigninScreen';
+import { useSelector } from 'react-redux';
+import RegisterScreen from './screens/RegisterScreen';
+import ProductsScreen from './screens/ProductsScreen';
+import ShippingScreen from './screens/ShippingScreen';
+import PaymentScreen from './screens/PaymentScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
+
+
+function App() {
+
+    const userSignin = useSelector(state => state.userSignin);
+    const {userInfo} = userSignin;
+
+  const openMenu = () => {
+    document.querySelector(".sidebar").classList.add("open")
+  }
+  const closeMenu = () => {
+    document.querySelector(".sidebar").classList.remove("open")
+  }
+  return (
+ <BrowserRouter>   
+    <div className="grid-container">
+        <header className="header">
+            <div className="brand">
+                <button onClick={openMenu}>
+                    &#9776;
+                </button>
+                <Link to='/'>ShopKart</Link>
+            </div>
+            <div className="header-links">
+                <Link to='/cart/:id?'>Cart </Link>
+                {
+                    userInfo ? <Link to='/profile'>{userInfo.name}</Link>:
+                    <Link to='/signin'>Sign In</Link>
+                }
+                 
+            </div>
+        </header>
+        <aside className="sidebar">
+            <h3>Shopping Categories</h3>
+            <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+            <ul>
+                <li>
+                    <a href="index.html">Pants</a>
+                </li>
+
+                <li>
+                    <a href="index.html">Shirts</a>
+                </li>
+            </ul>
+        </aside>
+        <main className="main">
+            <div className="content">
+                <Route path='/products' exact={true} component={ProductsScreen}/>
+                <Route path='/signin' exact={true} component={SigninScreen}/>
+                <Route path='/register' exact={true} component={RegisterScreen}/>
+                <Route path='/shipping' exact={true} component={ShippingScreen}/>
+                <Route path='/payment' exact={true} component={PaymentScreen}/>
+              <Route path='/product/:id'  component={ProductScreen} />
+              <Route path='/placeorder'  component={PlaceOrderScreen} />
+              <Route path='/cart/:id?' exact={true}  component= {CartScreen} />
+              <Route path='/' exact={true} component={HomeScreen} />
+               
+            </div>
+        </main>
+        <footer className="footer">
+            All rights reserved.
+        </footer>
+    </div>
+</BrowserRouter>  
+  );
+}
+
+export default App;
---
+++ fontend/src/data.js
@@ -0,0 +1,44 @@
+export default {
+        products:[
+            {
+            _id:'1',
+            name: 'Slim Shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10
+         },
+        {
+            _id:'2',
+            name: 'Fit Shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.0,
+            numReviews: 9
+         },
+         {
+            _id:'3',
+            name: 'Best Pants',
+            category: 'pants',
+            image: '/images/d1.jpg',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10
+         },
+         {
+            _id:'4',
+            name: 'Fit Shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.0,
+            numReviews: 9
+         }
+    ]
+}
